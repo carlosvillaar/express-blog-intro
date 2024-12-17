b54345db483bbd6b9ee1644c298841648336eb2c
carlosvillaar <carlosvillar004@gmail.com>
2024-12-17 17:25:46 +0100

    feat: exported the object and add a route that show a json file

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ const { log } = require('console');
 const express = require('express');
 const app = express();
 const port = 3000;
+const posts = require('./object');
 
 app.get('/', (req, res) => {
   res.send('Server del mio blog')
 })
 
 app.get('/bacheca', (req, res) => {
-  res.json()
-} )
+  res.json(posts)
+})
 
 app.listen(port, () => {})
